b14e9ed4c86deb7a0b1ef5199c937c9d54c0e8e6
Oliver <oliverfl@live.dk>
2016-01-06 15:55:39 +0100

    commit fix

---
+++ 02148_Project.Model/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02148_Project.Model
+{
+    class Message
+    {
+        public string Context { get; set; }
+
+        public string SenderName { get; set; }
+
+        public string RecieverName { get; set; }
+
+        public Boolean ToAll { get; set; }
+    }
+}
---
+++ Database tables/dbo.Chat.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Chat] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [Message]    VARCHAR (255) NOT NULL,
+    [SenderName]   VARCHAR(32)           NOT NULL,
+    [RecieverName] VARCHAR(32)           NOT NULL,
+    [ToAll]      BIT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Chat_Players] FOREIGN KEY ([SenderName]) REFERENCES [dbo].[Players] ([Name]),
+    CONSTRAINT [FK_Chat_Players_1] FOREIGN KEY ([RecieverName]) REFERENCES [dbo].[Players] ([Name])
+);
+
