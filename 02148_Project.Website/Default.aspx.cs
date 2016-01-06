17837431f6b78c654a917cccbddcaeeb3c7495db
Nina Stavnsbjerg <s144477@win.dtu.dk>
2016-01-06 13:50:46 +0100

    ...

--- 02148_Project.Website/Default.aspx.cs
+++ 02148_Project.Website/Default.aspx.cs
@@ -6,22 +6,23 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 namespace _02148_Project.Website
 {
 
     public partial class _Default : Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            //List<Resource> resources = new List<Resource>();
-            
-            //Add 5 resources
-            //for (int i = 1; i <= 5; i++)
-            //{
-            //    resources.Add(new Resource());
-            //}
-            //localResources.DataSource = resources;
-            //localResources.DataBind();
-        }
+        //protected void Page_Load(object sender, EventArgs e)
+        //{
+        //    List<ResourceOffer> resources = new List<ResourceOffer>();
+
+        //    Add 5 resources
+        //    for (int i = 1; i <= 5; i++)
+        //    {
+        //        resources.Add(new Resource());
+        //    }
+        //    localResources.DataSource = resources;
+        //    localResources.DataBind();
+        //}
     }
 }
