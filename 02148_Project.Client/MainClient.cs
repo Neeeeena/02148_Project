fd20b2086adedc41edb467bfa795d57d59e49c7f
Alemil <s134073@student.dtu.dk>
2016-01-08 13:03:33 +0100

    blah

--- 02148_Project.Client/MainClient.cs
+++ 02148_Project.Client/MainClient.cs
@@ -14,7 +14,7 @@ namespace _02148_Project.Client
         public List<ResourceOffer> allResourcesOnMarket;
         public List<TradeOffer> allTradeOffers;
         public List<Message> collectedMessages = new List<Message>();
-
+        
         public Player player;
 
 
@@ -50,14 +50,31 @@ namespace _02148_Project.Client
         // SERVER SKAL HAVE EN GetResourceFromMarked og så en UpdatePlayerTable hvor den -guld og +resource på en spiller.
 
 
+        // Own player gold and resource stuff
+        public void UpdateOwnGoldAndResources()
+        {
+            player = DatabaseInterface.ReadPlayer(player.Name);
+        }
+
 
         // Trade offer stuff:
 
-        public void GetAllTradeOffers()
+        public void ReadAllTradeOffers()
         {
             allTradeOffers = DatabaseInterface.ReadAllTradeOffers(player.Name);
         }
 
+        public void SendTradeOfferToPlayer(TradeOffer offer)
+        {
+            DatabaseInterface.PutTradeOffer(offer);
+        }
+
+        public void AcceptTradeOffer(int id)
+        {
+            TradeOffer offer = DatabaseInterface.GetTradeOffer(id);
+            Player sellerPlayer = DatabaseInterface.ReadPlayer(offer.SellerName);
+            //Update de 2 spillere
+        }
 
 
         // Message stuff:
@@ -65,13 +82,18 @@ namespace _02148_Project.Client
         public void GetNewMessage()
         {
             Message msg = DatabaseInterface.GetMessage(player.Name);
-            if(msg != null) collectedMessages.Add(msg);            
+            if(msg != null) collectedMessages.Add(msg);          
         }
 
         // Exception skal kastes
         public void SendNewMessage(Message msg)
         {
             DatabaseInterface.SendMessage(msg);
+        }
+
+        public void SendNewMessageToAll(Message msg)
+        {
+            //Send 1 besked til hver spiller, fx med x send message.
         }
     }
 }
