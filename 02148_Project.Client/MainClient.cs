e3b64e22503af7ade910a3e51830ffa685eb1c70
Alemil <s134073@student.dtu.dk>
2016-01-08 10:30:11 +0100

    Nye klasser i client

---
+++ 02148_Project.Client/MainClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _02148_Project;
+using _02148_Project.Model;
+
+namespace _02148_Project.Client
+{
+    public class MainClient
+    {
+
+        public List<ResourceOffer> allResourcesOnMarket;
+
+        //SKAL SÅDAN SET KALDES AF EN HANDLER, BARE LIGE FOR ORDEN I DESIGN OG HVORDAN DER SNAKKES SAMMEN
+        public void getAllResourcesOnMarket()
+        {
+            allResourcesOnMarket = CommunicationFunctionality.updateAllResourceOffers();
+        }
+    }
+}
