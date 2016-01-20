c605e7e8f802d827ca17fb99f729d514c2a7fd5e
Alemil <s134073@student.dtu.dk>
2016-01-20 09:17:23 +0100

    Rettet alt det Nina slettede :'(

--- 02148_Project.Website/Servers/Advert.cs
+++ 02148_Project.Website/Servers/Advert.cs
@@ -15,7 +15,7 @@ namespace _02148_Project.Website
 
         public int id { get; set; }
 
-        int stopwatchThreshold = 50000;
+        int stopwatchThreshold = 25000;
 
         //The timer is used for calling the event "closeAdvert(..)"
         System.Timers.Timer timer;
@@ -27,7 +27,7 @@ namespace _02148_Project.Website
 
             this.id = id;
             timer = new System.Timers.Timer();
-            timer.Interval = 60000;
+            timer.Interval = 30000;
             timer.AutoReset = false;
             timer.Elapsed += closeAdvert;
             timer.Enabled = true;
